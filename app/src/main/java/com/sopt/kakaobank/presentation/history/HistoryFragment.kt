c6d90caa925d2a5b6a031abd7dfb14992e7f0a5d
youjin09222 <youjin09222@gmail.com>
2024-05-17 23:31:09 +0900

    feat/#9: 뒤로 가기 구현

--- app/src/main/java/com/sopt/kakaobank/presentation/history/HistoryFragment.kt
+++ app/src/main/java/com/sopt/kakaobank/presentation/history/HistoryFragment.kt
@@ -1,5 +1,6 @@
 package com.sopt.kakaobank.presentation.history
 
+import androidx.navigation.fragment.findNavController
 import com.sopt.kakaobank.R
 import com.sopt.kakaobank.core.base.BindingFragment
 import com.sopt.kakaobank.core.util.fragment.statusBarColorOf
@@ -8,5 +9,13 @@ import com.sopt.kakaobank.databinding.FragmentHistoryBinding
 class HistoryFragment : BindingFragment<FragmentHistoryBinding>(R.layout.fragment_history) {
     override fun initView() {
         statusBarColorOf(R.color.main) //status 색상 변경
+        setBackButton()
+    }
+
+    // 뒤로 가기
+    private fun setBackButton() {
+        binding.btnHistoryBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
