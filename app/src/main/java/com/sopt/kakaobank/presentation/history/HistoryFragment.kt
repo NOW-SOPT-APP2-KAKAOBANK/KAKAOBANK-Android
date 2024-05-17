e288ff27ed2eca362db3754d213a49fbfd8d556d
youjin09222 <youjin09222@gmail.com>
2024-05-17 23:29:31 +0900

    feat/#9: status 색상 변경

--- app/src/main/java/com/sopt/kakaobank/presentation/history/HistoryFragment.kt
+++ app/src/main/java/com/sopt/kakaobank/presentation/history/HistoryFragment.kt
@@ -2,10 +2,11 @@ package com.sopt.kakaobank.presentation.history
 
 import com.sopt.kakaobank.R
 import com.sopt.kakaobank.core.base.BindingFragment
+import com.sopt.kakaobank.core.util.fragment.statusBarColorOf
 import com.sopt.kakaobank.databinding.FragmentHistoryBinding
 
 class HistoryFragment : BindingFragment<FragmentHistoryBinding>(R.layout.fragment_history) {
     override fun initView() {
-        // 로직 작성
+        statusBarColorOf(R.color.main) //status 색상 변경
     }
 }
