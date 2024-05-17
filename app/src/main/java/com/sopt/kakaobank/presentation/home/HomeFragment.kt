ddd0bdf75e9624d119c328325ee49cee6ea4351f
yskim6772 <yskim6772@gmail.com>
2024-05-17 15:14:56 +0900

    [FEAT] : recyclerview (HomeItem) 구현, item 간 간격 조정 추가

--- app/src/main/java/com/sopt/kakaobank/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/kakaobank/presentation/home/HomeFragment.kt
@@ -1,30 +1,49 @@
 package com.sopt.kakaobank.presentation.home
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
 import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.sopt.kakaobank.R
 import com.sopt.kakaobank.core.base.BindingFragment
 import com.sopt.kakaobank.databinding.FragmentHomeBinding
+import com.sopt.kakaobank.presentation.history.HistoryFragment
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
-    private lateinit var adapter: HomeItemAdapter
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.homeViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        val adapter = HomeItemAdapter(viewModel.homeItems)
+        binding.homeRecyclerView.layoutManager = LinearLayoutManager(context)
+        binding.homeRecyclerView.adapter = adapter
+
+        val spaceInPixels = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP, 9f, resources.displayMetrics
+        ).toInt()
+
+        binding.homeRecyclerView.addItemDecoration(object : RecyclerView.ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State
+            ) {
+                outRect.bottom = spaceInPixels
+            }
+        })
+    }
 
     override fun initView() {
         // 로직 작성
-        binding.homeRecyclerView.layoutManager = LinearLayoutManager(context)
-
-        val items = listOf(
-            HomeItem.CheckLimitItem("김미정님의 신용점수는"),
-            HomeItem.BankBookItem1("햄들의 통장"),
-            HomeItem.BankBookItem2("햄통장"),
-            HomeItem.BankBookItem3("김미정의 통장")
-        )
-
-        adapter = HomeItemAdapter(items)
-        binding.homeRecyclerView.adapter = adapter
     }
 
 }
