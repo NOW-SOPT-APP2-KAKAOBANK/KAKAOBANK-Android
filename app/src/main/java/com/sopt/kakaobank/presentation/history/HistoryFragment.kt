ae039225f7038f637443cf9b25ded54e2615a7f4
youjin09222 <youjin09222@gmail.com>
2024-05-17 23:43:52 +0900

    feat/#9: 1000원 단위에 콤마 추가

--- app/src/main/java/com/sopt/kakaobank/presentation/history/HistoryFragment.kt
+++ app/src/main/java/com/sopt/kakaobank/presentation/history/HistoryFragment.kt
@@ -7,6 +7,7 @@ import com.sopt.kakaobank.core.base.BindingFragment
 import com.sopt.kakaobank.core.util.fragment.statusBarColorOf
 import com.sopt.kakaobank.data.dto.response.History
 import com.sopt.kakaobank.databinding.FragmentHistoryBinding
+import java.text.DecimalFormat
 
 class HistoryFragment : BindingFragment<FragmentHistoryBinding>(R.layout.fragment_history) {
     override fun initView() {
@@ -22,82 +23,90 @@ class HistoryFragment : BindingFragment<FragmentHistoryBinding>(R.layout.fragmen
         }
     }
 
+    // 1000원 단위 콤마
+    private fun formatAmount(amount : Int): String {
+        val dec = DecimalFormat("#,###")
+        return dec.format(amount)
+    }
+
     private fun setHistoryList() {
+        val amount = 10000 //더미 데이터
+        val formattedAmount = formatAmount(amount)
         val historyAdapter = HistoryAdapter()
+
         binding.rvHistoryUsage.run {
             adapter = historyAdapter
             layoutManager = LinearLayoutManager(requireContext())
         }
-
-        historyAdapter.setHistoryList(mockHistoryList())
+        historyAdapter.setHistoryList(mockHistoryList(formattedAmount))
     }
 
     // 더미 데이터
-    private fun mockHistoryList() = listOf(
+    private fun mockHistoryList(amount: String) = listOf(
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         ),
         History(
             historyDate = "03.21",
             historyName = "김미정",
-            historySpent = "0원",
+            historySpent = amount,
             historyBalance = "0원"
         )
     )
