729892fa80dd1a6af10bf0b8656aee78b01c78c9
youjin09222 <youjin09222@gmail.com>
2024-05-17 23:11:21 +0900

    feat/#9: HistoryViewHolder 추가

---
+++ app/src/main/java/com/sopt/kakaobank/presentation/history/HistoryViewHolder.kt
@@ -0,0 +1,30 @@
+package com.sopt.kakaobank.presentation.history
+
+import androidx.recyclerview.widget.RecyclerView
+import com.sopt.kakaobank.data.dto.response.History
+import com.sopt.kakaobank.databinding.ItemHistoryDepositBinding
+import com.sopt.kakaobank.databinding.ItemHistoryWithdrawBinding
+
+// 입금
+class DepositViewHolder(private val binding: ItemHistoryDepositBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun onBind(historyData: History) {
+        binding.run {
+            tvHistoryDate.text = historyData.historyDate
+            tvHistoryName.text = historyData.historyName
+            tvHistorySpent.text = "-${historyData.historySpent}"
+            tvHistoryBalance.text = historyData.historyBalance
+        }
+    }
+}
+
+// 출금
+class WithdrawViewHolder(private val binding: ItemHistoryWithdrawBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun onBind(historyData: History) {
+        binding.run {
+            tvHistoryDate.text = historyData.historyDate
+            tvHistoryName.text = historyData.historyName
+            tvHistorySpent.text = historyData.historySpent
+            tvHistoryBalance.text = historyData.historyBalance
+        }
+    }
+}
