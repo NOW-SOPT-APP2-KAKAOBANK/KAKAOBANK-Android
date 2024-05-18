94ab7ff407a03aafa1d452b37b75022a9885cac9
Eonji-sw <ejkim0625@gmail.com>
2024-05-18 12:18:40 +0900

    [FEAT] : transfer activity 구현

--- app/src/main/java/com/sopt/kakaobank/presentation/transfer/TransferActivity.kt
+++ app/src/main/java/com/sopt/kakaobank/presentation/transfer/TransferActivity.kt
@@ -2,10 +2,43 @@ package com.sopt.kakaobank.presentation.transfer
 
 import com.sopt.kakaobank.R
 import com.sopt.kakaobank.core.base.BindingActivity
+import com.sopt.kakaobank.core.util.context.statusBarColorOf
 import com.sopt.kakaobank.databinding.ActivityTransferBinding
+import com.sopt.kakaobank.presentation.transfer.bottomsheet.TransferBottomSheet
 
 class TransferActivity : BindingActivity<ActivityTransferBinding>(R.layout.activity_transfer) {
     override fun initView() {
-        // 로직 작성
+        statusBarColorOf(R.color.white0)
+        initTransferAdapter()
+        initCloseBtnClickListener()
+        initEnterBtnClickListener()
+    }
+
+    private fun initTransferAdapter() {
+        binding.rvTransferRecentTransfer.adapter = TransferAdapter(click = { _, _ -> }).apply {
+            submitList(
+                listOf(
+                    TransferEntity("김윤서", "농협 1234567890", false),
+                    TransferEntity("박유진", "카카오뱅크 1234567890", false)
+                )
+            )
+        }
+        binding.rvTransferRecentTransfer.addItemDecoration(TransferItemDecorator(this))
+    }
+
+    private fun initCloseBtnClickListener() {
+        binding.appbarTransfer.tvTransferAppbarClose.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun initEnterBtnClickListener() {
+        binding.layoutTransferEnter.setOnClickListener {
+            TransferBottomSheet().show(supportFragmentManager, TRANSFER_BOTTOM_SHEET)
+        }
+    }
+
+    companion object {
+        const val TRANSFER_BOTTOM_SHEET = "transfer_bottom_sheet"
     }
 }
