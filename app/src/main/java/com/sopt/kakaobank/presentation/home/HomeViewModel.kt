56eb0a41da0ae21084ce5ed69bed3d9fea6554e6
yskim6772 <yskim6772@gmail.com>
2024-05-23 01:30:53 +0900

    [CHORE] : 더미값 수정

--- app/src/main/java/com/sopt/kakaobank/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/kakaobank/presentation/home/HomeViewModel.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
     val homeItems: List<HomeItem> = listOf(
         HomeItem.CheckLimitItem("김윤서"),
-        HomeItem.BankBookItem1("융들의 통장", "0"),
-        HomeItem.BankBookItem2("융통장", "0"),
-        HomeItem.BankBookItem3("김윤서의 통장", "41","3000041")
+        HomeItem.BankBookItem1("융들의 통장", "0원"),
+        HomeItem.BankBookItem2("융통장", "0원"),
+        HomeItem.BankBookItem3("김윤서의 통장", "41원","3000041원")
     )
 }
