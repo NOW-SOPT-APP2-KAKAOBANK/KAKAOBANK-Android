dc914f38b7550cb5dbc1ae1ee1e07dcaaa48e1eb
youjin09222 <youjin09222@gmail.com>
2024-05-23 00:24:02 +0900

    refactor/#9: RecyclerView.Adapter에서 ListAdapter로 변경

--- app/src/main/java/com/sopt/kakaobank/presentation/history/HistoryAdapter.kt
+++ app/src/main/java/com/sopt/kakaobank/presentation/history/HistoryAdapter.kt
@@ -2,42 +2,36 @@ package com.sopt.kakaobank.presentation.history
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.sopt.kakaobank.core.view.ItemDiffCallback
 import com.sopt.kakaobank.data.dto.response.History
 import com.sopt.kakaobank.databinding.ItemHistoryDepositBinding
 import com.sopt.kakaobank.databinding.ItemHistoryWithdrawBinding
 
-class HistoryAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    // 임시의 빈 리스트
-    private var historyList: List<History> = emptyList()
-
+class HistoryAdapter : ListAdapter<History, RecyclerView.ViewHolder>(HistoryAdapterDiffCallback) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
-            // 입금
-            TYPE_DEPOSIT -> {
-                val binding = ItemHistoryDepositBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-                DepositViewHolder(binding)
-            }
-            // 출금
-            TYPE_WITHDRAW -> {
-                val binding = ItemHistoryWithdrawBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-                WithdrawViewHolder(binding)
-            }
+            TYPE_DEPOSIT -> ItemHistoryDepositBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ).let { DepositViewHolder(it) }
+            TYPE_WITHDRAW -> ItemHistoryWithdrawBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ).let { WithdrawViewHolder(it) }
             else -> throw IllegalArgumentException("Invalid view type")
         }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val profile = historyList[position]
+        val profile = getItem(position)
 
         when (holder) {
-            is DepositViewHolder -> {
-                holder.onBind(profile)
-            }
-            is WithdrawViewHolder -> {
-                holder.onBind(profile)
-            }
+            is DepositViewHolder -> holder.onBind(profile)
+            is WithdrawViewHolder -> holder.onBind(profile)
         }
     }
 
@@ -48,16 +42,14 @@ class HistoryAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         }
     }
 
-    fun setHistoryList(historyList: List<History>) {
-        this.historyList = historyList.toList()
-        notifyDataSetChanged()
-    }
-
-    override fun getItemCount(): Int = historyList.size
-
     companion object {
         const val NUMBER_EVEN = 0
         const val TYPE_DEPOSIT = 0
         const val TYPE_WITHDRAW = 1
+
+        private val HistoryAdapterDiffCallback = ItemDiffCallback<History>(
+            onItemsTheSame = { old, new -> old.historyName == new.historyName },
+            onContentsTheSame = { old, new -> old == new }
+        )
     }
 }
