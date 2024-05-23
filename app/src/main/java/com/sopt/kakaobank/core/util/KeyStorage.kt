77e073ab61fb14c6d844bfd541b1c35bab46fb1f
yskim6772 <yskim6772@gmail.com>
2024-05-24 03:59:07 +0900

    [CHORE] : 상수화 KeyStorage로 이동

--- app/src/main/java/com/sopt/kakaobank/core/util/KeyStorage.kt
+++ app/src/main/java/com/sopt/kakaobank/core/util/KeyStorage.kt
@@ -3,6 +3,7 @@ package com.sopt.kakaobank.core.util
 object KeyStorage {
     const val API = "api"
     const val V1 = "v1"
+    const val ACCOUNT_LIST = "account-list"
     const val RECENT_TRANSFERS = "recent-transfers"
     const val ACCOUNT_ID = "accountId"
     const val MY_ACCOUNT_ID = "myAccountId"
