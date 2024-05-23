c214dc23aa913783de505a1484888fc2d739261b
Eonji-sw <ejkim0625@gmail.com>
2024-05-23 17:11:53 +0900

    [FEAT] : transfer viewModel 적용

--- app/src/main/java/com/sopt/kakaobank/presentation/transfer/TransferActivity.kt
+++ app/src/main/java/com/sopt/kakaobank/presentation/transfer/TransferActivity.kt
@@ -1,27 +1,54 @@
 package com.sopt.kakaobank.presentation.transfer
 
+import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.sopt.kakaobank.R
 import com.sopt.kakaobank.core.base.BindingActivity
 import com.sopt.kakaobank.core.util.context.statusBarColorOf
+import com.sopt.kakaobank.core.view.UiState
 import com.sopt.kakaobank.databinding.ActivityTransferBinding
 import com.sopt.kakaobank.presentation.transfer.bottomsheet.TransferBottomSheet
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import timber.log.Timber
 
 class TransferActivity : BindingActivity<ActivityTransferBinding>(R.layout.activity_transfer) {
+    private val transferViewModel by viewModels<TransferViewModel>()
+
     override fun initView() {
         statusBarColorOf(R.color.white0)
-        initTransferAdapter()
+        initGetRecentObserve()
         initCloseBtnClickListener()
         initEnterBtnClickListener()
     }
 
-    private fun initTransferAdapter() {
-        binding.rvTransferRecentTransfer.adapter = TransferAdapter(click = { _, _ -> }).apply {
-            submitList(
-                listOf(
-                    Transfer(R.drawable.ic_toss_bank, "김윤서", "토스 1234567890", false),
-                    Transfer(R.drawable.ic_nong_bank, "박유진", "농협 1234567890", false)
-                )
-            )
+    private fun initGetRecentObserve() {
+        transferViewModel.getTransferRecent.flowWithLifecycle(lifecycle).onEach {
+            when (it) {
+                is UiState.Success -> initTransferAdapter(it.data)
+                is UiState.Failure -> Timber.d("실패 : $it")
+                is UiState.Loading -> Timber.d("로딩중")
+            }
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun onBookmarkClick(
+        markedAccountId: Int,
+        status: Boolean,
+    ) {
+        if (status) {
+            // 코드 구현 : 윤서
+        } else {
+            transferViewModel.postTransferBookmark(markedAccountId)
+        }
+    }
+
+    private fun initTransferAdapter(accountData: List<Transfer>) {
+        binding.rvTransferRecentTransfer.adapter = TransferAdapter(
+            onClickBookmarkBtn = ::onBookmarkClick,
+        ).apply {
+            submitList(accountData)
         }
         binding.rvTransferRecentTransfer.addItemDecoration(TransferItemDecorator(this))
     }
